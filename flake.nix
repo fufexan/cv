032508adbae078a2ec9e18d9057f5db6c6ba1861
Mihai Fufezan <fufexan@protonmail.com>
2023-07-17 18:02:10 +0300

    flake: use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,32 +1,50 @@
 {
   description = "Mihai Fufezan Resume";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["aarch64-linux" "aarch64-darwin" "x86_64-linux" "x86_64-darwin"];
 
-  outputs = { self, nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        p = import nixpkgs { inherit system; };
-        tex-env = p.texlive.combine {
-          inherit (p.texlive) scheme-medium lipsum fmtcount datetime;
+      perSystem = {pkgs, ...}: let
+        tex-env = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-medium lipsum fmtcount datetime;
         };
-      in
-      {
-        devShells.default = p.mkShell rec {
-          packages = [ tex-env ];
+
+        mkDate = longDate:
+          with builtins; (concatStringsSep "-" [
+            (substring 0 4 longDate)
+            (substring 4 2 longDate)
+            (substring 6 2 longDate)
+          ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [tex-env];
           name = "LaTeX";
         };
-        packages.default = p.stdenv.mkDerivation {
-          name = "fufexan_resume";
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "fufexan_resume";
+          version = mkDate (self.lastModifiedDate or "19700101");
           src = self;
 
-          buildInputs = [ tex-env ];
+          buildInputs = [tex-env];
 
           buildPhase = "latexmk -pdf";
           installPhase = "mkdir -p $out/; mv *.pdf $out/";
         };
-      });
+      };
+    };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
 }
